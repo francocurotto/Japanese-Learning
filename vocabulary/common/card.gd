5ab8cbdfd271c5f4379b529994bb982167e71b91
Franco Curotto <francocurotto@gmail.com>
2024-09-20 21:57:18 -0300

    start colors

--- vocabulary/common/card.gd
+++ vocabulary/common/card.gd
@@ -1,23 +1,28 @@
 @tool
 extends PanelContainer
 
+@export_file("*.svg") var icon :
+    set(_icon):
+        icon = _icon
+        $Column/Icon.texture = load(icon)
+
 @export var english_text: String = "English":
     set(_english_text):
         english_text = _english_text
-        %English.text = english_text
+        $Column/English.text = english_text
 
 @export var kana_text: String = "かな":
     set(_kana_text):
         kana_text = _kana_text
-        %Kana.text = kana_text
+        $Column/Kana.text = kana_text
         
 @export var kanji_text: String = "漢字":
     set(_kanji_text):
         kanji_text = _kanji_text
-        %Kanji.text = kanji_text
+        $Column/Kanji.text = kanji_text
 
 @export var show_kana = false:
     set(_show_kana):
         show_kana = _show_kana
-        %Kana.visible = show_kana
-        %Kanji.visible = not show_kana
+        $Column/Kana.visible = show_kana
+        $Column/Kanji.visible = not show_kana
